15f7999dcfe263457e70fed1c84a4e1d76b98d61
anicexp-wq <anicexp@gmail.com>
2026-09-08 22:01:05 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Cache "network-first" : l'appli se met à jour dès qu'il y a du réseau,
 // et reste utilisable hors ligne au bord du terrain.
-const CACHE = 'grez-coach-v18';
+const CACHE = 'grez-coach-v19';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './logo.png'];
 
 self.addEventListener('install', e => {
